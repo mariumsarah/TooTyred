e3f60441d95342ec49bf941d73fb82e8c7207ce2
mariumsarah <sarahm13654@gmail.com>
2019-02-11 23:18:18 +0400

    search filter

--- optrApp/views.py
+++ optrApp/views.py
@@ -100,6 +100,42 @@ def bikes(request):
                 i=i+1
             return HttpResponse(json.dumps(response_date),
                 content_type="application/json")
+        elif request.POST.get('formtype','') == 'viewreservations':
+            response_date={}
+            response_date[0]=0;
+            reservations = Reservation.objects.raw("select r.fine_desc as fine_desc, r.fine_cost as fine_cost,br.bor_bike_id as bor_bike_id,br.bor_reservation_id as bor_reservation_id, r.res_type as res_type,r.feedback as feedback,r.c_id as c_id,r.reservation_id as reservation_id,r.res_code as res_code,r.res_cost as res_cost,r.res_date as res_date,r.starttime as starttime,r.endtime as endtime,r.c_rating as c_rating,ss.name as startname,sd.name as endname from bike_on_reservation as br,reservation as r,stationroutes as sr,station_on_reservation as sor,station as ss,station as sd where br.bor_reservation_id=r.reservation_id and sor.sor_reservation_id=r.reservation_id and sor.sor_route_id=sr.route_id and sr.start_station_id=ss.station_id and sr.end_station_id=sd.station_id")
+            i=1
+            for each in reservations:
+                response_date[i]={}
+                response_date[i]['bike_id']=each.bor_bike_id
+                response_date[i]['reservation_id']=each.bor_reservation_id
+                response_date[i]['res_code']=each.res_code
+                response_date[i]['res_cost']=str(each.res_cost)
+                response_date[i]['res_type']=each.res_type.res_type_name
+                response_date[i]['res_date']=each.res_date.strftime('%d %b %Y %I:%M %p')
+                response_date[i]['starttime']=each.starttime.strftime('%d %b %Y %I:%M %p')
+                response_date[i]['endtime']=each.endtime.strftime('%d %b %Y %I:%M %p')
+                if(each.c_rating!=None):
+                    response_date[i]['c_rating']=each.c_rating.rating_value
+                    response_date[i]['feedback']=each.feedback
+                response_date[i]['c_id']=each.c_id
+                if(each.fine_cost!=None):
+                    response_date[i]['fine_desc']=each.fine_desc
+                    response_date[i]['fine_cost']=str(each.fine_cost)
+                response_date[i]['startname']=each.startname
+                response_date[i]['endname']=each.endname
+                response_date[i]['bikes_on_reservation']={}
+                bikes = Bike.objects.raw('select * from bike as b, bike_on_reservation as br where br.bor_reservation_id=%s and b.bike_id=br.bor_bike_id',(each.reservation_id,))
+                x=0
+                for n in bikes:
+                    response_date[i]['bikes_on_reservation'][x]={}
+                    response_date[i]['bikes_on_reservation'][x]['bike_id']=n.bike_id
+                    response_date[i]['bikes_on_reservation'][x]['bike_type']=n.bike_type.bike_type
+                    response_date[i]['bikes_on_reservation'][x]['bike_image']=n.bike_type.bike_image
+                    x=x+1
+                i=i+1
+            return HttpResponse(json.dumps(response_date),
+                content_type="application/json")
     else:
         return render(request, 'operator/inspectbikes.html',{'allbikes':Bike.objects.all()})
 
